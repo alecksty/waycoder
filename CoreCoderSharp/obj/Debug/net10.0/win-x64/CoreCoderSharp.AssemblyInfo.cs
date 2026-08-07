f30e5a244f9e65c28c5c0f509adf9854bc2c056a
施探宇 <alecksty@163.com>
2026-08-07 20:17:50 +0800

    We need to generate a concise git commit message, one line, English, <70

--- CoreCoderSharp/obj/Debug/net10.0/win-x64/CoreCoderSharp.AssemblyInfo.cs
+++ CoreCoderSharp/obj/Debug/net10.0/win-x64/CoreCoderSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("corecoder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fcaced65bda9780a4548779cb81891f19b53ea9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46b1dd881c45763080feeba9dfad24939f086a03")]
 [assembly: System.Reflection.AssemblyProductAttribute("corecoder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("corecoder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
