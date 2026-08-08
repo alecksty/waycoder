76ab576dc999b6a9fca5a98af61ae4c197ecd53c
施探宇 <alecksty@163.com>
2026-08-08 20:48:09 +0800

    We need to generate a git commit message for the given modified files. T

--- CoreCoderSharp/obj/Release/net10.0/win-x64/CoreCoderSharp.AssemblyInfo.cs
+++ CoreCoderSharp/obj/Release/net10.0/win-x64/CoreCoderSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("corecoder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff36c9844648a1a5c2a2b79555b2fed66baebc76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb9bff857db442f757c154f28d71d36d2be44a68")]
 [assembly: System.Reflection.AssemblyProductAttribute("corecoder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("corecoder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
