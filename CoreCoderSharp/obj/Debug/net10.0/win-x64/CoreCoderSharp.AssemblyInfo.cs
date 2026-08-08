b83b8f35cdd7ab982b8e7efa37b91e72aae2eb5a
施探宇 <alecksty@163.com>
2026-08-08 16:30:43 +0800

    We need to generate a concise git commit message for modified files. The

--- CoreCoderSharp/obj/Debug/net10.0/win-x64/CoreCoderSharp.AssemblyInfo.cs
+++ CoreCoderSharp/obj/Debug/net10.0/win-x64/CoreCoderSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("corecoder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab685728b765c9272e67e43a6acbbbd616a9a4ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8599c14c01253983ac9fe60549f9250da4c536ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("corecoder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("corecoder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
